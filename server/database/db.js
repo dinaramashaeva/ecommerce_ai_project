9b1e3f30b11d1587451c9e28a04d669d5e0263bf
dinaramashaeva <110571827+dinaramashaeva@users.noreply.github.com>
2025-11-22 06:45:20 +0600

    initial commit

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+import pkg from "pg";
+const { Client } = pkg;
+
+const database = new Client({
+  user: "postgres",
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: "12345678",
+  port: process.env.DB_PORT,
+});
+
+try {
+  await database.connect();
+  console.log("Connected to the database successfully");
+} catch (error) {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+}
+
+export default database;
