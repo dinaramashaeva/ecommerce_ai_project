868961d3c4e51f943bf2e26addf57e7c7e0cd8f2
dinaramashaeva <110571827+dinaramashaeva@users.noreply.github.com>
2025-11-22 16:42:43 +0600

    backend commit

--- server/database/db.js
+++ server/database/db.js
@@ -3,10 +3,10 @@ const { Client } = pkg;
 
 const database = new Client({
   user: "postgres",
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  database: "mern_ecommerce_store ",
   password: "12345678",
-  port: process.env.DB_PORT,
+  port: 5432,
 });
 
 try {
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,21 +1,18 @@
-import nodeMailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    service: process.env.SMTP_SERVICE,
-    port: process.env.SMTP_PORT,
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      pass: process.env.SMTP_PASSWORD, // Gmail App Password
     },
   });
 
-  const mailOptions = {
+  await transporter.sendMail({
     from: process.env.SMTP_MAIL,
     to: email,
     subject,
     html: message,
-  };
-  await transporter.sendMail(mailOptions);
+  });
 };
